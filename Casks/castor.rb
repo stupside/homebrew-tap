508ab77fd961623b5bf4bbf3456abc845d864f9d
castor <git@castor.dev>
2026-07-19 02:36:13 +0000

    Brew cask update for castor 1.4.1

--- Casks/castor.rb
+++ Casks/castor.rb
@@ -1,9 +1,9 @@
 cask "castor" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.4.0"
-  sha256 arm:   "1624a5e7c0834dcad669e831bc8949506bd6bb1ce8352bf2ee646810394686ae",
-         intel: "6612371eb92859cd051465d16a32917ba6ffafbf7394f5730faa83eaea645de4"
+  version "1.4.1"
+  sha256 arm:   "0654e7574f6eb8c9deaf459e4a0d277af6c66f11f6370dacca279fdf83572cec",
+         intel: "d3cca086a105e566a6291386c1a2d205f7e7819869342141907cb6c07530ea89"
 
   url "https://github.com/stupside/castor/releases/download/v#{version}/castor_#{version}_darwin_#{arch}.tar.gz"
   name "castor"
