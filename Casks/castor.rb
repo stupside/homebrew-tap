07dedd2fc965343a481ca8aa08fcbccc493f0106
castor <git@castor.dev>
2026-07-19 17:53:19 +0000

    Brew cask update for castor 1.4.3

--- Casks/castor.rb
+++ Casks/castor.rb
@@ -1,9 +1,9 @@
 cask "castor" do
   arch arm: "arm64", intel: "amd64"
 
-  version "1.4.2"
-  sha256 arm:   "6fff0fafc4757a69dee474e72f6271fd6c96757ba02e3f13c7754be3352755de",
-         intel: "532fced9654cbf8427d43937fa8a128dd5a53a1432cfe23f45e61d1ce648f942"
+  version "1.4.3"
+  sha256 arm:   "eb4aa590840646f786adb4e1eb5252125917ced7b01a333413e97ddbecf74350",
+         intel: "bbed9429b4750aaac2ae364596d055cdd6daa0cec4b4a7cd59df99a3726d8b04"
 
   url "https://github.com/stupside/castor/releases/download/v#{version}/castor_#{version}_darwin_#{arch}.tar.gz"
   name "castor"
@@ -11,6 +11,12 @@ cask "castor" do
   homepage "https://github.com/stupside/castor"
 
   binary "castor"
+
+  # The binary is unsigned, so macOS Gatekeeper quarantines it on download and
+  # refuses to run it. Strip the quarantine attribute so it works out of the box.
+  postflight do
+    system_command "/usr/bin/xattr", args: ["-cr", "#{staged_path}/castor"]
+  end
 
   zap trash: "~/.castor/config.yml"
 end
